5bf8fdb4e35684f1b0d5752f9b5947839a5e4740
Dilan Manatunga <manatunga@gmail.com>
2016-04-11 15:39:00 -0400

    Began data structure implementation of RVM

--- rvm.cpp
+++ rvm.cpp
@@ -1,56 +1,202 @@
 #include "rvm.h"
+#include <stdio.h>
+#include <fstream>
 
-rvm_t rvm_init(const char* directory) {
-  return nullptr;
-}
-
-void* rvm_map(rvm_t rvm, const char* segname, int size_to_create) {
+void* Rvm::MapSegment(std::string segname, size_t size_to_create) {
+  std::unordered_map<std::string, RvmSegment*>::iterator segment = name_to_segment_map_.find(segname);
   // Check to see if the segment has already been mapped
-  std::unordered_map<char*, void*>::iterator segment = rvm->segment_map.find(segname);
-  if (segment == rvm->segment_map.end()) {
+  if (segment == name_to_segment_map_.end()) {
+    // Map the segment from the disk
+    std::string segpath = construct_segment_path(segname);
+    std::ifstream segbacking(segpath);
+    void* segbase;
+    size_t segsize = size_to_create;
+    if (segbacking.good()) {
+      // Backing file exists, so read it in
+      segbase = nullptr;
+    } else {
+      segbase = malloc(segsize);
+      if (segbase == NULL) {
+        perror("Malloc Failed");
+        exit(-1);
+      }
+    }
 
+    // Create the segment and insert mappings for segment
+    RvmSegment* rvm_segment = new RvmSegment(segname, segpath, segbase, segsize);
+    name_to_segment_map_[segname] = rvm_segment;
+    base_to_segment_map_[segbase] = rvm_segment;
+    return segbase;
   } else {
     // Trying to re-map a segment that has already been mapped
+#if DEBUG
+    std::cout << "Segment " << segname << " already mapped." << std::endl;
+#endif
     return (void*) -1;
   }
-
-  return nullptr;
 }
 
-void rvm_unmap(rvm_t rvm, void* segbase) {
-
-}
-
-void rvm_destroy(rvm_t rvm, const char* segname) {
-  // Check to see if the segment is mapped
-  std::unordered_map<char*, void*>::iterator segment = rvm->segment_map.find(segname);
-  if (segment == rvm->segment_map.end()) {
-
+void Rvm::UnmapSgement(void* segbase) {
+  std::unordered_map<void*, RvmSegment*>::iterator segment = base_to_segment_map_.find(segbase);
+  if (segment == base_to_segment_map_.end()) {
+    // Error: Segment does not exist
+#if DEBUG
+    std::cerr << "Error: Invalid Segment Base (" << segbase << ")" << std::endl;
+#endif
+    return;
   } else {
+    RvmSegment* rvm_segment = segment->second;
+
+#if DEBUG
+    assert(segbase == rvm_segment->base_);
+#endif
+
+    free(rvm_segment->base_);
+    name_to_segment_map_.erase(rvm_segment->name_);
+    base_to_segment_map_.erase(rvm_segment->base_);
+    delete rvm_segment;
+  }
+}
+
+void Rvm::DestroySegment(std::string segname) {
+  // Check to see if the segment is mapped
+  std::unordered_map<std::string, RvmSegment*>::iterator segment = name_to_segment_map_.find(segname);
+  if (segment == name_to_segment_map_.end()) {
+    std::string segpath = construct_segment_path(segname);
+    if (remove(segpath.c_str()) != 0) {
+#if DEBUG
+      perror("Error deleting file");
+#endif
+    }
+  } else {
+#if DEBUG
+    std::cout << "Cannot Destroy: Segment " <<< segname << " already mapped." << std::endl;
+#endif
     // Trying to destroy a segment that is currently mapped
     return;
   }
 }
 
-trans_t rvm_begin_trans(rvm_t rvm, int numsegs, void** segbases) {
+trans_t Rvm::BeginTransaction(int numsegs, void** segbases) {
+  // Check to see that input segment bases are valid
   for (int i = 0; i < numsegs; i++) {
-    // Check if a segment is already being modified by another transaction
+    std::unordered_map<void*, RvmSegment*>::iterator iterator = base_to_segment_map_.find(segbases[i]);
+    // Make sure segment exists
+    if (iterator != base_to_segment_map_.end()) {
+      RvmSegment* rvm_segment = iterator->second;
+      // Check if segment is already owned by another transaction
+      if (rvm_segment->HasOwner()) {
+        return (trans_t)-1;
+      }
+    } else {
+      return (trans_t)-1;
+    }
   }
-  return 0;
+
+  // Create the transaction
+  trans_t tid = g_trans_id++;
+  RvmTransaction* rvm_trans = new RvmTransaction(tid, this);
+  g_trans_list[tid] = rvm_trans;
+  for (int i = 0; i < numsegs; i++) {
+    rvm_trans->AddSegment(base_to_segment_map_[segbases[i]]);
+  }
+  return tid;
+}
+
+void Rvm::TruncateLog() {
+
+}
+
+void RvmTransaction::AboutToModify(void* segbase, int offset, int size) {
+
+}
+
+void RvmTransaction::Commit() {
+
+}
+
+void RvmTransaction::Abort() {
+
+}
+
+void RvmTransaction::AddSegment(RvmSegment* segment) {
+  base_to_segment_map_[segment->base_] = segment;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Library functions (passthrough calls)
+///////////////////////////////////////////////////////////////////////////////
+rvm_t rvm_init(const char* directory) {
+  if (directory == NULL) {
+    return nullptr;
+  }
+
+  Rvm* rvm  = new Rvm(directory);
+  return rvm;
+}
+
+void* rvm_map(rvm_t rvm, const char* segname, int size_to_create) {
+  if (size_to_create <= 0) {
+    return (void*) -1;
+  }
+  return rvm->MapSegment(std::string(segname), (size_t) size_to_create);
+}
+
+void rvm_unmap(rvm_t rvm, void* segbase) {
+  rvm->UnmapSgement(segbase);
+}
+
+void rvm_destroy(rvm_t rvm, const char* segname) {
+  rvm->DestroySegment(std::string(segname));
+}
+
+trans_t rvm_begin_trans(rvm_t rvm, int numsegs, void** segbases) {
+  return rvm->BeginTransaction(numsegs, segbases);
 }
 
 void rvm_about_to_modify(trans_t tid, void* segbase, int offset, int size) {
-
+  std::unordered_map<trans_t, RvmTransaction*>::iterator iter = g_trans_list.find(tid);
+  if (iter != g_trans_list.end()) {
+    RvmTransaction* rvm_trans = iter->second;
+    rvm_trans->AboutToModify(segbase, offset, size);
+  } else {
+#if DEBUG
+    std::cout << "about_to_modify(): Inputted Invalid Transaction " << tid << std::endl;
+#endif
+  }
 }
 
 void rvm_commit_trans(trans_t tid) {
-
+  std::unordered_map<trans_t, RvmTransaction*>::iterator iter = g_trans_list.find(tid);
+  if (iter != g_trans_list.end()) {
+    RvmTransaction* rvm_trans = iter->second;
+    rvm_trans->Commit();
+  } else {
+#if DEBUG
+    std::cout << "Committing Invalid Transaction " << tid << std::endl;
+#endif
+  }
 }
 
 void rvm_abort_trans(trans_t tid) {
-
+  std::unordered_map<trans_t, RvmTransaction*>::iterator iter = g_trans_list.find(tid);
+  if (iter != g_trans_list.end()) {
+    RvmTransaction* rvm_trans = iter->second;
+    rvm_trans->Abort();
+  } else {
+#if DEBUG
+    std::cout << "Aborting Invalid Transaction " << tid << std::endl;
+#endif
+  }
 }
 
 void rvm_truncate_log(rvm_t rvm) {
-
+  rvm->TruncateLog();
 }
+
+
+
+
+
+
--- rvm_internal.h
+++ rvm_internal.h
@@ -4,13 +4,82 @@
 #include <vector>
 #include <unordered_map>
 
-struct rvm {
-  char* directory;
-  std::unordered_map<char*, void*> segment_map;
+class Rvm;
+typedef Rvm* rvm_t;
+typedef int trans_t;
+
+std::unordered_map<trans_t, RvmTransaction*> g_trans_list;
+static trans_t g_trans_id = 0;
+
+class RvmSegment {
+  friend class Rvm;
+  friend class RvmTransaction;
+
+ public:
+  RvmSegment(std::string segname, std::string segpath, void* segbase, size_t segsize)
+          : name_(segname), path_(segpath), base_(segbase), size_(segsize), owned_by_(nullptr) {};
+  ~RvmSegment() {};
+
+  RvmTransaction* GetOwner() {
+    return owned_by_;
+  }
+
+  void SetOwner(RvmTransaction* owner) {
+    owned_by_ = owner;
+  }
+
+  bool HasOwner() {
+    return owned_by_ != nullptr;
+  }
+
+ private:
+  std::string name_;
+  std::string path_;
+  void* base_;
+  size_t size_;
+  RvmTransaction* owned_by_;
 };
 
-typedef struct rvm* rvm_t;
-typedef int trans_t;
+class Rvm {
+ public:
+  Rvm(const char* directory) : directory_(directory) {};
+  ~Rvm() {};
+
+  void* MapSegment(std::string segname, size_t size_to_create);
+  void UnmapSgement(void* segbase);
+  void DestroySegment(std::string segname);
+
+  trans_t BeginTransaction(int numsegs, void** segbases);
+  void TruncateLog();
+ private:
+  std::string directory_;
+  std::unordered_map<std::string, RvmSegment*> name_to_segment_map_;
+  std::unordered_map<void*, RvmSegment*> base_to_segment_map_;
+
+ private:
+  std::string construct_segment_path(std::string segname) {
+    return directory_ + "seg_" + segname;
+  }
+};
+
+class RvmTransaction {
+ public:
+  RvmTransaction(trans_t tid, Rvm* rvm) : id_(tid), rvm_(rvm) {};
+
+  void AboutToModify(void* segbase, int offset, int size);
+  void Commit();
+  void Abort();
+  void AddSegment(RvmSegment* segment);
+
+ private:
+  trans_t id_;
+  Rvm* rvm_;
+  std::unordered_map<void*, RvmSegment*> base_to_segment_map_;
+};
+
+
+
+
 
 
 #endif // RVM_INTERNAL_H
