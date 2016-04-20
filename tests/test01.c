e6997194dd0e3316133e6e1f38ef03c7b633c9c1
Dilan Manatunga <manatunga@gmail.com>
2016-04-20 15:54:16 -0400

    Refactored test01

--- tests/test01.c
+++ tests/test01.c
@@ -1,58 +1,47 @@
 #include "rvm.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <stdio.h>
+#include <sys/stat.h>
 
-/* proc1 writes some data, commits it, then exits */
-void proc1() {
-  rvm_t rvm;
-  char* segs[1];
+#define TEST_STRING "hello, world"
+#define OFFSET2 1000
 
-  rvm = rvm_init("rvm_segments");
-  rvm_destroy(rvm, "testseg01");
-  segs[0] = (char*) rvm_map(rvm, "testseg01", 10000);
-
-  // this should fail
-  rvm_destroy(rvm, "testseg01");
-  abort();
-
-  rvm_unmap(rvm, segs[0]);
-}
-
-void proc2() {
-  rvm_t rvm;
-  char* segs[1];
-
-  rvm = rvm_init("rvm_segments");
-  rvm_destroy(rvm, "testseg01");
-  segs[0] = (char*) rvm_map(rvm, "testseg01", 10000);
-
-  rvm_unmap(rvm, segs[0]);
-
-  // this should not fail
-  rvm_destroy(rvm, "testseg01");
-
-  return;
+int file_exists(const char* name) {
+  struct stat buffer;
+  return stat(name, &buffer) == 0;
 }
 
 int main(int argc, char** argv) {
-  int pid;
+  rvm_t rvm;
+  trans_t trans;
+  char* segs[1];
 
-  pid = fork();
-  if (pid < 0) {
-    perror("fork");
-    exit(2);
+  rvm = rvm_init("rvm_segments");
+  rvm_destroy(rvm, "testseg01");
+  segs[0] = (char*) rvm_map(rvm, "testseg01", 10000);
+
+  trans = rvm_begin_trans(rvm, 1, (void**) segs);
+
+  rvm_about_to_modify(trans, segs[0], 0, 100);
+  sprintf(segs[0], TEST_STRING);
+
+  rvm_about_to_modify(trans, segs[0], OFFSET2, 100);
+  sprintf(segs[0] + OFFSET2, TEST_STRING);
+
+  rvm_commit_trans(trans);
+
+  // This should fail
+  rvm_destroy(rvm, "testseg01");
+
+  rvm_unmap(rvm, segs[0]);
+
+  rvm_truncate_log(rvm);
+
+  if (file_exists("rvm_segments/seg_testseg01.rvm")) {
+    printf("OK\n");
+  } else {
+    printf("ERROR: Segment file no longer present");
   }
-  if (pid == 0) {
-    proc1();
-    exit(0);
-  }
-
-  waitpid(pid, NULL, 0);
-
-  proc2();
 
   return 0;
 }
