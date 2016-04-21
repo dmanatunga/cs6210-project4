a0f781a44d63de4d27a8ed6f90dd661dbbe06697
Pranith Kumar <bobby.prani@gmail.com>
2016-04-21 00:12:04 -0400

    Add truncate test

---
+++ tests/test14.c
@@ -0,0 +1,71 @@
+/*
+ * Test that truncate_log() works
+ */
+
+#include "rvm.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define TEST_STRING "hello, world"
+#define OFFSET2 1000
+
+
+/* proc1 writes some data, commits it, then exits */
+void proc1() {
+  rvm_t rvm;
+  trans_t trans;
+  char* segs[1];
+
+  rvm = rvm_init("rvm_segments");
+  rvm_destroy(rvm, "testseg");
+  segs[0] = (char*) rvm_map(rvm, "testseg", 10000);
+
+
+  trans = rvm_begin_trans(rvm, 1, (void**) segs);
+
+  rvm_about_to_modify(trans, segs[0], 0, 100);
+  sprintf(segs[0], TEST_STRING);
+
+  rvm_about_to_modify(trans, segs[0], OFFSET2, 100);
+  sprintf(segs[0] + OFFSET2, TEST_STRING);
+
+  rvm_commit_trans(trans);
+
+  rvm_truncate_log(rvm);
+
+  struct stat buffer;
+
+  if (stat("rvm_segments/redo_log.rvm", &buffer) == 0) {
+    if (buffer.st_size == 0) {
+      printf("OK\n");
+    } else {
+      fprintf(stderr, "ERROR: truncate failed\n");
+    }
+  } else {
+    fprintf(stderr, "ERROR: truncate failed\n");
+  }
+
+  abort();
+}
+
+int main(int argc, char** argv) {
+  int pid;
+
+  pid = fork();
+  if (pid < 0) {
+    perror("fork");
+    exit(2);
+  }
+  if (pid == 0) {
+    proc1();
+    exit(0);
+  }
+
+  waitpid(pid, NULL, 0);
+
+  return 0;
+}
