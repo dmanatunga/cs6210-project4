8d9dc979a394c66f89911d2fdb588585c1f0396a
Pranith Kumar <bobby.prani@gmail.com>
2016-04-22 21:26:57 -0400

    Add default tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 make
+LD_LIBRARY_PATH=../ ./basic
+LD_LIBRARY_PATH=../ ./abort
+LD_LIBRARY_PATH=../ ./multi-abort
+LD_LIBRARY_PATH=../ ./multi
+LD_LIBRARY_PATH=../ ./truncate
 
 for i in `seq 24`; do
   printf -v i "%02d" $i
